3522ec0cfd6994e8e0c9d04b5fa945dcfde297b8
MrLyrad <103238972+MrLyrad@users.noreply.github.com>
2024-06-19 17:25:36 +0800

    Update mySmartContract.sol

--- mySmartContract.sol
+++ mySmartContract.sol
@@ -1,29 +1,40 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-
 contract ErrHandle {
     // declaring the initial value of the token supply
-    uint private  tokenSupply = 0;
+    uint private tokenSupply = 0;
+    uint private subCounter = 0;
 
-    // this require error handling statement requires the input to be divisible by 5
-    function requireErrHandle(uint _i) public {
+    function addTokens(uint _i) public {
+        // the assert function here ensures that the input is not 0
+        assert(_i > 0);    
+
+        // this require error handling statement requires the input to be divisible by 5
         require(_i % 5 == 0, "Input must be divisible by 5!");
         tokenSupply += _i;
     }
 
-    // this revert error handling statement reverts back any gas used if the input is zero or a negative number
-    function revertErrHandle(uint _i) public {
-        if(_i == 0) {
-            revert("Input must not be zero!");
-        }
+    function subTokens(uint _i) public {
+        // the assert function here ensures that the input is not 0
+        assert(_i > 0);
+
+        require(_i % 5 == 0 && _i != 0, "Input must be divisible by 5!");
         tokenSupply -= _i;
+        subCounter += 1;
+
+        if(subCounter > 2) {
+            revert("Subtraction Limit Reached!");
+        }
     }
 
-    // this assert error handling statement checks if the input is 1, meaning yes/confirm
-    function assertErrHandle(uint _i) external view returns(uint){
-        assert(_i == 1);
+    function showTokens () external view returns(uint) {
         return tokenSupply;
+    }
+
+    function numSub () external view returns(uint) {
+        return subCounter;
     }
 
 }
