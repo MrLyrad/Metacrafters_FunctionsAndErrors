13ca2a1a6bfba0a3bcf171ee9b6fa9b06a4c3800
MrLyrad <103238972+MrLyrad@users.noreply.github.com>
2024-06-19 17:53:59 +0800

    Update mySmartContract.sol

--- mySmartContract.sol
+++ mySmartContract.sol
@@ -3,28 +3,29 @@
 pragma solidity 0.8.18;
 
 contract ErrHandle {
-    // declaring the initial value of the token supply
+    // declaring the initial value of the token supply and the subtraction counter
     uint private tokenSupply = 0;
-    uint private subCounter = 0;
+    uint private subCounter = 2;
 
     function addTokens(uint _i) public {
-        // the assert function here ensures that the input is not 0
-        assert(_i > 0);    
+        // the assert function here ensures that the input is not more than 1000
+        assert(_i < 1000);    
 
-        // this require error handling statement requires the input to be divisible by 5
+        // this require error handling statement requires the input to be divisible by 5 but not equal to 0
         require(_i % 5 == 0, "Input must be divisible by 5!");
         tokenSupply += _i;
     }
 
     function subTokens(uint _i) public {
-        // the assert function here ensures that the input is not 0
-        assert(_i > 0);
+        // the assert function here ensures that the input is not more than 1000
+        assert(_i < 1000);
 
+        // this require error handling statement requires the input to be divisible by 5 but not equal to 0
         require(_i % 5 == 0 && _i != 0, "Input must be divisible by 5!");
         tokenSupply -= _i;
-        subCounter += 1;
+        subCounter -= 1;
 
-        if(subCounter > 2) {
+        if(subCounter < 0) {
             revert("Subtraction Limit Reached!");
         }
     }
